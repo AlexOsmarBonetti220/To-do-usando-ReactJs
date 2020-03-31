e368efe5d185ed66543091f4427ef85fe5f8f8a1
alex-bonetti <62677325+alex-bonetti@users.noreply.github.com>
2020-03-31 15:33:24 -0300

    O projeto será um to-do online. Criação da adição e listagem de tarefas

--- src/components/screens/telaInicial.js
+++ src/components/screens/telaInicial.js
@@ -6,8 +6,25 @@ import {useHistory} from "react-router-dom";
 
 export default function TelaInicial(){
     let history = useHistory();
-    const [uid, setUid] = useState(0);
     const [nome, setNome] = useState("");
+    const [todo, setTodo] = useState([]);
+    const [tarefas, setTarefas] = useState([]);
+    const [uid, setUid] = useState(0);
+    const changeToDo = (e) => {
+        setTodo({
+            ...todo,
+            [e.currentTarget.name]:e.currentTarget.value
+        })
+    }
+    const addToDo = (e) => {
+        let userTodo = firebase.database().ref("todo").child(uid);
+        let key = userTodo.push().key;
+        userTodo.child(key).set({
+            todoName: todo.todoName,
+            todoDescription: todo.todoDescription
+        })
+        e.preventDefault();
+    }
 
     const signOut = () => {
         firebase.auth().signOut();
@@ -18,19 +35,59 @@ export default function TelaInicial(){
             if(!user){
                 history.push("/");
             }else{
-                setUid(user.uid);
+                let uid = user.uid;
+                setUid(uid);
                 firebase.database().ref("users").child(user.uid).on("value", (snapshot)=>{
                     let nome = snapshot.val().nome
                     setNome(nome);
+                })
+                //Pegando os dados das tarefas no firebase
+                firebase.database().ref("todo").child(user.uid).on("value", (snapshot)=>{
+                    snapshot.forEach((child)=>{
+                        //Coloco no array
+                        tarefas.push({
+                            todoName:child.val().todoName,
+                            todoDescription:child.val().todoDescription,
+                            todoKey:child.key
+                        })
+                    })
+                    setTarefas([...tarefas, tarefas]);
+                },error => {
+                    alert(error);
                 })
             }
         })
     }, [])
     return(
         <div>
-            <h2>Tela Inicial</h2>
-            <h3>Seu uid: {uid}</h3>
-            <h3>Seu nome: {nome}</h3>
+            <div>
+                <h1>To do online</h1>
+                <h3>Bem vindo, {nome}</h3>
+            </div>
+            <div>
+                <form onSubmit={addToDo}>
+                    <div>
+                        <label htmlFor="todoName">Digite o nome da tarefa: </label>
+                        <input type="text" id="todoName" value={todo.todoName} name="todoName" onChange={changeToDo} /><br/><br/>
+                        <label htmlFor="todoDescription">Digite a descrição da tarefa: </label><br/>
+                        <textarea id="todoDescription" value={todo.todoDescription} name="todoDescription" onChange={changeToDo}></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div>
+            <div>
+                {tarefas.map((tarefa)=>{
+                    return(
+                        <div key={tarefa.todoKey}>
+                            <h4>{tarefa.todoKey}</h4>
+                            <h3>{tarefa.todoName}</h3>
+                            <p>{tarefa.todoDescription}</p>
+                        </div>
+                    )
+                })}
+            </div>
             <button type="button" onClick={signOut}>Sair</button>
         </div>
     )
